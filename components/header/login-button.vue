39bd0e09274f08daa5500748070af441d5a72b8d
Georg Wittberger <georg.wittberger@gmail.com>
2020-08-21 14:38:20 +0200

    Return to previous page after login

--- components/header/login-button.vue
+++ components/header/login-button.vue
@@ -1,12 +1,16 @@
 <template>
-  <nuxt-link to="/login" class="btn btn-secondary">Login</nuxt-link>
+  <button @click="navigateToLoginPage" class="btn btn-secondary">Login</button>
 </template>
 
 <script lang="ts">
 import { Vue, Component } from 'nuxt-property-decorator'
 
 @Component
-export default class LoginButton extends Vue {}
+export default class LoginButton extends Vue {
+  navigateToLoginPage() {
+    this.$auth.redirect('login')
+  }
+}
 </script>
 
 <style></style>
