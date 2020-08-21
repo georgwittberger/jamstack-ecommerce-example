12ec891eed3fb64eddbfe0d6b1985c3120de49f3
Georg Wittberger <georg.wittberger@gmail.com>
2020-08-21 14:20:11 +0200

    Add user profile page

--- components/header/user-menu.vue
+++ components/header/user-menu.vue
@@ -1,6 +1,7 @@
 <template>
   <ul>
     <li>Logged in as: {{ $auth.user.name }}</li>
+    <li><nuxt-link to="/user/profile">My User Profile</nuxt-link></li>
     <li><a @click="logout" href="#">Logout</a></li>
   </ul>
 </template>
---
+++ pages/user/profile.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h1>My User Profile</h1>
+    <p>Name: {{ $auth.user.name }}</p>
+    <p>E-Mail Address: {{ $auth.user.email }}</p>
+    <p>Phone Number: {{ $auth.user.phone_number }}</p>
+    <p v-if="$auth.user.address">
+      Local Address:
+      {{ $auth.user.address.street_address }},
+      {{ $auth.user.address.postal_code }} {{ $auth.user.address.locality }},
+      {{ $auth.user.address.country }}
+    </p>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'nuxt-property-decorator'
+
+@Component({
+  middleware: 'auth',
+})
+export default class UserProfilePage extends Vue {
+  head() {
+    return { title: 'My User Profile' }
+  }
+}
+</script>
+
+<style></style>
